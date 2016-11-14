dab8fb666e4c83d960609f53cd315121ddbd29bc
Elena Creangă <elena.creanga.mail@gmail.com>
2016-11-14 21:35:47 +0200

    Add dependency injection

---
+++ Solution/ImageCollectionExpander/ICE.Infrastructure/AutofacConfig.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace ICE.Infrastructure
+{
+    public class AutofacConfig
+    {
+        public void RegisterDependencies(ContainerBuilder builder)
+        {
+            //new Register("Repository", typeof(_DefaultRepository_).Assembly).Bind(builder);
+        }
+    }
+}
