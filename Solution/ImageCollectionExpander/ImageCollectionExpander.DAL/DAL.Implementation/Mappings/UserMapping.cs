4b706e9f384c745535b6adc56735fd30a580f6c1
Gabriela Neagu <gabriela.neagu20@gmail.com>
2016-11-14 22:09:37 +0200

    Basic DAL config

---
+++ Solution/ImageCollectionExpander/ImageCollectionExpander.DAL/DAL.Implementation/Mappings/UserMapping.cs
@@ -0,0 +1,19 @@
+﻿using ImageCollectionExpander.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCollectionExpander.DAL.DAL.Implementation.Mappings
+{
+    public class UserMapping : EntityTypeConfiguration<User>
+    {
+        UserMapping()
+        {
+            this.ToTable("User");
+
+        }
+    }
+}
