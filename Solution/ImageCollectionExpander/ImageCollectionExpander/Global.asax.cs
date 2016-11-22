63ce5edd100c98061399712246bb5a8ecd525211
Elena Creangă <elena.creanga.mail@gmail.com>
2016-11-22 02:15:41 +0200

    Add tests for rename collection

--- Solution/ImageCollectionExpander/ImageCollectionExpander/Global.asax.cs
+++ Solution/ImageCollectionExpander/ImageCollectionExpander/Global.asax.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ImageCollectionExpander.App_Start;
 
 namespace ImageCollectionExpander
 {
@@ -13,6 +10,7 @@ namespace ImageCollectionExpander
     {
         protected void Application_Start()
         {
+            ContainerConfig.RegisterContainer();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
